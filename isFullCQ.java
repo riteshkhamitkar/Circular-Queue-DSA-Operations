b4440bf8e5a026844d7f02dd008df10b271280d1
riteshkhamitkar <89683381+riteshkhamitkar@users.noreply.github.com>
2022-07-24 15:44:29 +0530

    Update isFullCQ.java

--- isFullCQ.java
+++ isFullCQ.java
@@ -1,36 +1,50 @@
-public class CircularQueue {
-    int [] arr;
-    int topOfQueue;
-    int beginningOfQueue;
-    int size;
+.................
+Main.java
+.................
+class Main{
+    public static void main(String[] args){
+        CircularQueue newCQ = new CircularQueue(3);
+        boolean result = newCQ.isFull();
+        System.out.println(result);
 
-    public CircularQueue(int size){
-        this.arr = new int[size];
-        this.size = size;
-        this.topOfQueue = -1;
-        this.beginningOfQueue = -1;
-        System.out.println("The CQ is created successfully! " + size + " size!");
-    }
-
-    //isEmpty
-
-    public boolean isEmpty(){
-        if(topOfQueue==-1){
-            return true;
-        }else{
-            return false;
-        }
-    }
-
-    //isFull 
-
-    public boolean isFull(){
-        if(topOfQueue+1 == beginningOfQueue){
-            return true;
-        }else if(beginningOfQueue == 0 && topOfQueue == size){
-            return true;
-        }else{
-            return false;
-        }
     }
 }
+..................................
+CircularQueue.java
+..................................
+public class CircularQueue {
+  int[] arr;
+  int topOfQueue;
+  int beginningOfQueue;
+  int size;
+
+  public CircularQueue(int size) {
+    this.arr = new int[size];
+    this.size = size;
+    this.topOfQueue = -1;
+    this.beginningOfQueue = -1;
+    System.out.println("The CQ is successfully created with size of " + size);
+  }
+
+  // isEmpty
+  public boolean isEmpty() {
+    if (topOfQueue == -1) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  // isFull
+  public boolean isFull() {
+    if (topOfQueue+1 == beginningOfQueue) {
+      return true;
+    } else if (beginningOfQueue==0 && topOfQueue+1==size) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+...........
+//////finish///////////
